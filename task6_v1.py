03aa56edc94777bfa61c5aebc78901314c4596cf
AnikinVladislav <vlad9anikin1999@gmail.com>
2022-12-09 21:39:14 +0300

    add show and find functions

--- task6_v1.py
+++ task6_v1.py
@@ -1,6 +1,5 @@
 farmers = {}
-reviews = {}
-
+reviews = {'fmid': [], 'name': [], 'surname': [], 'rate': [], 'text': []}
 
 with open('Export.csv', 'r') as f:
     for data in f:
@@ -8,27 +7,81 @@ with open('Export.csv', 'r') as f:
         # print(data_row)
         # data_row[8] - City; data_row[10] - State; data_row[11] - zip_data
         farmers[data_row[0]] = data_row[1:]
-# print(farmers)
-print(farmers['1019863'][9])
-city = input('Введите город:')
-state = input('Введите штат:')
-frm_interests = []
-for fmid in farmers:
-    market = farmers[fmid]
-    market_porj = [market[0], market[7], market[9], market[10], market[19], market[20]]
-    if market_porj[1] == city and market_porj[2] == state:
-        frm_interests.append(fmid)
 
-want_review = input('Хотите оставить рецензию на эти рынки (Да\Нет):')
+while(True):
+    command = int(input('Выберете действие:\n 1.Посмотреть все фермерские рынки\n 2.Поиск фермерского рынка\n 3.Отставить реценизию\n 4. Выход\n'))
+    if command < 1 or command > 4:
+        print('Такой команды нет.')
+    
+    if command == 1:
+        print('Все фермерские рынки:')
+        for farm in farmers:
+            print(farmers[farm])
 
-if want_review.capitalize() == 'Да':
-    reviews['name'] = input('Введите свое имя:')
-    reviews['surname'] = input('Введите свою фамилию:')
-    reviews['markerts_fmid'] = frm_interests
-    print('Реценизруемые рынки:\n')
-    for frm_interest in frm_interests:
-        market_rev_porj = [farmers[frm_interest][0], farmers[frm_interest][7], farmers[frm_interest][9], farmers[frm_interest][10], farmers[frm_interest][19], farmers[frm_interest][20]]
-        print(market_rev_porj)
-    rate = int(input('Введите рейтинг (от 1 до 5):'))
-    reviews['rate'] = (lambda x: 5 if x > 5 else(1 if x < 1 else x))(rate)
-print(reviews)
+        print('\n\nВсе рецензии:')
+        if reviews == {}:
+            print('Реценизий еще нет.\n\n')
+        else:
+            for review in reviews:
+                print(review)
+    
+    if command == 2:
+        command_search = int(input('По каким параметрам вы хотите найти фермерские рынки:\n 1.По городу и штату\n 2.Почтовый индекс\n'))
+        if command < 1 or command > 2:
+            print('Такого метода поиска нет, выберите метод из предложенных.')
+
+        if command_search == 1:
+            city = input('Введите город:')
+            state = input('Введите штат:')
+            search_fmid = []
+            for fmid in farmers:
+                market = farmers[fmid]
+                market_porj = [market[0], market[7], market[9], market[10], market[19], market[20]]
+                if market_porj[1] == city and market_porj[2] == state:
+                    search_fmid.append(fmid)
+                    print(f'\nID рынка:{fmid} Название рынка:{market_porj[0]} Город и штат рынка:{market_porj[1], market_porj[2]} Почтовый индекс:{market_porj[3]} Координаты:({market_porj[4]}, {market_porj[5]})')
+            
+        if command_search == 2:
+            wanted_fmid = input('Введите почтовый индекс:')
+            search_fmid = []
+            for fmid in farmers:
+                market = farmers[fmid]
+                market_porj = [market[0], market[7], market[9], market[10], market[19], market[20]]
+                if market_porj[3] == wanted_fmid:
+                    search_fmid.append(fmid)
+                    print(f'\nID рынка:{fmid} Название рынка:{market_porj[0]} Город и штат рынка:{market_porj[1], market_porj[2]} Почтовый индекс:{market_porj[3]} Координаты:({market_porj[4]}, {market_porj[5]})')
+
+        if search_fmid == [] and (command_search == 1 or command_search == 2):
+            print('Рынок не найден')
+        else:
+            want_more_info = input('Хотите больше информации о рынке (Да\Нет):')
+            if want_more_info == 'Да':
+                for fmid in search_fmid:
+                    print(farmers[fmid])
+
+    if command == 3:
+            review_fmid = input('Введите fmid инетерисуемого рынка')    
+            if review_fmid not in farmers.keys():
+                print('Такого ID не существует')
+            else:
+                review_name = input('Введите свое имя:')
+                review_surname = input('Введите свою фамилию:')
+                rate = int(input('Введите рейтинг (от 1 до 5):'))
+                review_rate = (lambda x: 5 if x > 5 else(1 if x < 1 else x))(rate)
+                review_text = input('Напишите рецензию')
+                reviews['fmid'].append(review_fmid)
+                reviews['name'].append(review_name)
+                reviews['surname'].append(review_surname)
+                reviews['rate'].append(rate)
+                reviews['text'].append(review_text)
+    
+    if command == 4:
+        break
+
+
+
+
+    
+ 
+ 
+
