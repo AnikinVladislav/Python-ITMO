12913c9f848d42fdc8be05caeb37d21ed2d59bdc
ToughMelomaniac <114314923+ToughMelomaniac@users.noreply.github.com>
2023-01-21 22:25:07 +0300

    added view_console

---
+++ View_console.py
@@ -0,0 +1,101 @@
+import MySQLdb
+import model
+
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="000000",     # your password
+                     db="farmers")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+# Create table for review authors
+cur.execute("CREATE TABLE IF NOT EXISTS author (authorId INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL)")
+
+# Create table for reviews
+cur.execute("CREATE TABLE IF NOT EXISTS reviews (rate TINYINT UNSIGNED NOT NULL, review TEXT, authorId INT NOT NULL, FMID INT UNSIGNED NOT NULL, FOREIGN KEY(authorId) REFERENCES author(authorId), FOREIGN KEY(FMID) REFERENCES frms_markets(FMID) ) ")
+
+
+
+frms_list = model.readAllMarkets(cur)
+
+while (True):
+
+    command = input('Выберете действие:\n 1.Посмотреть все фермерские рынки\n 2.Поиск фермерского рынка\n 3.Оставить реценизию\n 4. Выход\n')
+
+    if command == '1':
+        print('Все фермерские рынки:')
+        model.showAllMarkets(frms_list)
+
+        print('\n\nВсе рецензии:')
+        review_list = model.readAllReviews(cur)
+        if review_list == []:
+            print('Рецензий еще нет\n')
+        else:
+            model.showAllReviews(review_list)
+
+    elif command == '2':
+        command_search = input('По каким параметрам вы хотите найти фермерские рынки:\n 1.По городу и штату\n 2.Почтовый индекс\n 3.На определенном растоянии от рынка\n')
+
+        if command_search == '1':
+            city = input('Введите город:')
+            state = input('Введите штат:')
+            search_list = model.srchBycityandstate(frms_list, city, state)
+            if search_list == []:
+                print('Рынок не найден\n')
+            else:
+                model.showAllMarkets(search_list)
+
+        elif command_search == '2':
+            wanted_zip = input('Введите почтовый индекс:')
+            search_list = model.srchByZip(frms_list, wanted_zip)
+            model.showAllMarkets(search_list)
+            if search_list == []:
+                print('Рынок не найден\n')
+            else:
+                model.showAllMarkets(search_list)
+
+        elif command_search == '3':
+            pass
+            # R = input('Введите расстояние поиска в милях:')
+            # search_list = model.srchByArea(frms_list, market,R)
+            # model.showAllMarkets(search_list)
+
+        else:
+            print('Такого метода поиска нет, выберите метод из предложенных\n')
+
+
+        # want_more_info = input('Хотите больше информации о рынке (Да\Нет):')
+        # if want_more_info == 'Да':
+        #     for fmid in search_fmid:
+        #         print(farmers[fmid])
+
+    elif command == '3':
+        review_fmid = int(input('Введите fmid интересуещего рынка: '))
+        if model.chekOccurFmid(frms_list, review_fmid):
+            print('Такой ID существует')
+
+            #     review_name = input('Введите свое имя:')
+            #     review_surname = input('Введите свою фамилию:')
+            #     rate = int(input('Введите рейтинг (от 1 до 5):'))
+            #     review_rate = (lambda x: 5 if x > 5 else (1 if x < 1 else x))(rate)
+            #     review_text = input('Напишите рецензию')
+            #     reviews['fmid'].append(review_fmid)
+            #     reviews['name'].append(review_name)
+            #     reviews['surname'].append(review_surname)
+            #     reviews['rate'].append(rate)
+            #     reviews['text'].append(review_text)
+
+        else:
+            print('Такого ID не существует')
+
+    elif command == '4':
+        break
+
+    else:
+        print('Такой команды нет.')
+
+
+db.close()
---
+++ model.py
@@ -0,0 +1,181 @@
+class FarmerMarket():
+
+    def __init__(self, data):
+        # FMID,MarketName,Website,Facebook,Twitter,Youtube,OtherMedia,street,city,\
+        # County,State,zip,Season1Date,Season1Time,Season2Date,Season2Time,Season3Date,Season3Time,\
+        # Season4Date,Season4Time,x,y,Location,Credit,WIC,WICcash,SFMNP,SNAP,Organic,Bakedgoods,Cheese,\
+        # Crafts,Flowers,Eggs,Seafood,Herbs,Vegetables,Honey,Jams,Maple,Meat,Nursery,Nuts,Plants,Poultry,\
+        # Prepared,Soap,Trees,Wine,Coffee,Beans,Fruits,Grains,Juices,Mushrooms,PetFood,Tofu,WildHarvested,updateTime
+
+        # function for processing farmer's market products
+        def preprocess(parametr):
+            if parametr == 'Y':
+                return 1
+            elif parametr == 'N':
+                return 0
+            else:
+                return 2
+
+        self.FMID = data[0]
+        self.MarketName = data[1]
+        self.Website = data[2]
+        self.Facebook = data[3]
+        self.Twitter = data[4]
+        self.Youtube = data[5]
+        self.OtherMedia = data[6]
+        self.street = data[7]
+        self.city = data[8]
+        self.County = data[9]
+        self.State = data[10]
+        self.zip = data[11]
+        self.Season1Date = data[12]
+        self.Season1Time = data[13]
+        self.Season2Date = data[14]
+        self.Season2Time = data[15]
+        self.Season3Date = data[16]
+        self.Season3Time = data[17]
+        self.Season4Date = data[18]
+        self.Season4Time = data[19]
+        self.x = data[20]
+        self.y = data[21]
+        self.Location = data[22]
+        self.Credit = preprocess(data[23])
+        self.WIC = preprocess(data[24])
+        self.WICcash = preprocess(data[25])
+        self.SFMNP = preprocess(data[26])
+        self.SNAP = preprocess(data[27])
+        self.Organic = preprocess(data[28])
+        self.Bakedgoods = preprocess(data[29])
+        self.Cheese = preprocess(data[30])
+        self.Crafts = preprocess(data[31])
+        self.Flowers = preprocess(data[32])
+        self.Eggs = preprocess(data[33])
+        self.Seafood = preprocess(data[34])
+        self.Herbs = preprocess(data[35])
+        self.Vegetables = preprocess(data[36])
+        self.Honey = preprocess(data[37])
+        self.Jams = preprocess(data[38])
+        self.Maple = preprocess(data[39])
+        self.Meat = preprocess(data[40])
+        self.Nursery = preprocess(data[41])
+        self.Nuts = preprocess(data[42])
+        self.Plants = preprocess(data[43])
+        self.Poultry = preprocess(data[44])
+        self.Prepared = preprocess(data[45])
+        self.Soap = preprocess(data[46])
+        self.Trees = preprocess(data[47])
+        self.Wine = preprocess(data[48])
+        self.Coffee = preprocess(data[49])
+        self.Beans = preprocess(data[50])
+        self.Fruits = preprocess(data[51])
+        self.Grains = preprocess(data[52])
+        self.Juices = preprocess(data[53])
+        self.Mushrooms = preprocess(data[54])
+        self.PetFood = preprocess(data[55])
+        self.Tofu = preprocess(data[56])
+        self.WildHarvested = preprocess(data[57])
+        self.updateTime = data[58]
+
+    def __str__(self):
+        return f'({self.FMID} , {self.MarketName} , {self.Website} , {self.Facebook} , {self.Twitter} , {self.Youtube} , {self.OtherMedia} , \
+{self.street} , {self.city} , {self.County} , {self.State} , {self.zip} , {self.Season1Date} , {self.Season1Time} , {self.Season2Date} , \
+{self.Season2Time} , {self.Season3Date} , {self.Season3Time} , {self.Season4Date} , {self.Season4Time} , {self.x} , {self.y} , {self.Location} , \
+{self.Credit} , {self.WIC} , {self.WICcash} , {self.SFMNP} , {self.SNAP} , {self.Organic} , {self.Bakedgoods} , {self.Cheese} , \
+{self.Crafts} , {self.Flowers} , {self.Eggs} , {self.Seafood} , {self.Herbs} , {self.Vegetables} , {self.Honey} , {self.Jams} , \
+{self.Maple} , {self.Meat} , {self.Nursery} , {self.Nuts} , {self.Plants} , {self.Poultry} , {self.Prepared} , {self.Soap} , \
+{self.Trees} , {self.Wine} , {self.Coffee} , {self.Beans} , {self.Fruits} , {self.Grains} , {self.Juices} , {self.Mushrooms} , \
+{self.PetFood} , {self.Tofu} , {self.WildHarvested} , {self.updateTime})'
+
+    def main_info(self):
+        return f'({self.FMID} , {self.MarketName} , {self.city} , {self.County} , {self.State} , {self.zip} , {self.x} , {self.y})'
+
+
+class review():
+
+    def __init__(self, fmid, authid, rate, comment=""):
+        self.fmid = fmid
+        self.authorid = authid
+        self.rate = rate
+        self.comm = comment
+
+    def __str__(self):
+        return f'fmid = {self.fmid}, authorid = {self.authorid}, rate = {self.rate}, comment = {self.comm}'
+
+    def add_to_DB(self, cur, db):
+        sql = "INSERT INTO reviews (rate, review, authorId, FMID) VALUES (%s, %s, %s, %s)"
+        val = (self.rate, self.comm, self.authorid, self.fmid)
+        cur.execute(sql, val)
+        db.commit()
+
+
+class user():
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def add_to_DB(self, cur, db):
+        sql = "INSERT INTO author (name, surname) VALUES (%s, %s)"
+        val = (self.name, self.surname)
+        cur.execute(sql, val)
+        db.commit()
+
+    def get_author_id(self, cur):
+        sql = "SELECT a.authorId FROM author a WHERE a.name = %s and a.surname = %s"
+        val = (self.name, self.surname)
+        cur.execute(sql, val)
+        return cur.fetchone()[0]
+
+
+def srchBycityandstate(fermers_list: FarmerMarket, city, state):
+    temp_list = []
+    for i in range(len(fermers_list)):
+        if fermers_list[i].city == city and fermers_list[i].State == state:
+            temp_list.append(fermers_list[i])
+    return temp_list
+
+def srchByZip(fermers_list: FarmerMarket, zip):
+    temp_list = []
+    for i in range(len(fermers_list)):
+        if fermers_list[i].zip == zip:
+            temp_list.append(fermers_list[i])
+    return temp_list
+
+
+def srchByArea(fermers_list: FarmerMarket, market: FarmerMarket, R):
+    temp_list = []
+    for i in range(len(fermers_list)):
+        if ((fermers_list[i].x - market.x) ** 2 + (fermers_list[i].y - market.y) ** 2) <= R ** 2:
+            temp_list.append(fermers_list[i])
+    return temp_list
+
+def chekOccurFmid(fermers_list: FarmerMarket, fmid):
+    for i in range(len(fermers_list)):
+        if fermers_list[i].FMID == fmid:
+            return True
+    return False
+
+def showAllMarkets(fermers_list: FarmerMarket):
+    for elem in fermers_list:
+        print(elem.main_info())
+
+
+def readAllMarkets(cur):
+    frms_list = []
+    cur.execute("SELECT * FROM frms_markets")
+    for row in cur.fetchall():
+        temp = FarmerMarket(row)
+        frms_list.append(temp)
+    return frms_list
+
+def showAllReviews(review_list: review):
+    for elem in review_list:
+        print(elem)
+
+def readAllReviews(cur):
+    review_list = []
+    cur.execute("SELECT * FROM reviews")
+    for row in cur.fetchall():
+        temp = review(row[3], row[2], row[0], row[1])
+        review_list.append(temp)
+    return review_list
