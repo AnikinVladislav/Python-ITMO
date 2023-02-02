6e869be63d22a3818df3cbec7919730ddf6920ad
ToughMelomaniac <114314923+ToughMelomaniac@users.noreply.github.com>
2023-02-03 01:09:55 +0300

    Added to View_UI search by city and state and zip

--- View_UI.py
+++ View_UI.py
@@ -1,5 +1,6 @@
 import Script_UI
 import ETL
+import model
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
@@ -7,35 +8,56 @@ class Ui_FarmerMarkets(Script_UI.Ui_MainWindow):
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
         self.add_function()
+        self.search_function()
     def add_function(self):
         self.ButtonViewAllMarkets.clicked.connect(lambda: self.ShowAllMarkets())
 
     def ShowAllMarkets(self):
+        fermMarketsList = ETL.getAllMarkets()
+        self.ShowMarkets(fermMarketsList)
+
+    def ShowMarkets(self, fermMarketList):
+        if fermMarketList != []:
+            self.MarketTable.setRowCount(len(fermMarketList))
+            self.MarketTable.setColumnCount(len(fermMarketList[0].columnNameMain))
+            self.MarketTable.setHorizontalHeaderLabels(fermMarketList[0].columnNameMain)
+
+            for i in range(len(fermMarketList)):
+                for j in range(len(fermMarketList[0].columnNameMain)):
+                    if j == 0:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(str(fermMarketList[i].FMID)))
+                    elif j == 1:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(fermMarketList[i].MarketName))
+                    elif j == 2:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(fermMarketList[i].city))
+                    elif j == 3:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(fermMarketList[i].County))
+                    elif j == 4:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(fermMarketList[i].State))
+                    elif j == 5:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(str(fermMarketList[i].zip)))
+                    elif j == 6:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(f"{fermMarketList[i].x:.1f}"))
+                    elif j == 7:
+                        self.MarketTable.setItem(i, j, QtWidgets.QTableWidgetItem(f"{fermMarketList[i].y:.1f}"))
+            self.MarketTable.resizeColumnsToContents()
+        else:
+            self.MarketTable.clear()
+
+    def search_function(self):
+        self.ButtonFindMarkets.clicked.connect(lambda: self.searchMarkets())
+
+    def searchMarkets(self):
         fermMarketList = ETL.getAllMarkets()
-        self.MarketTable.setRowCount(len(fermMarketList))
-        self.MarketTable.setColumnCount(len(fermMarketList[0].columnNameMain))
-        self.MarketTable.setHorizontalHeaderLabels(fermMarketList[0].columnNameMain)
-
-        for i in range(len(fermMarketList)):
-            for j in range(len(fermMarketList[0].columnNameMain)):
-                if j == 0:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(str(fermMarketList[i].FMID)))
-                elif j == 1:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(fermMarketList[i].MarketName))
-                elif j == 2:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(fermMarketList[i].city))
-                elif j == 3:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(fermMarketList[i].County))
-                elif j == 4:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(fermMarketList[i].State))
-                elif j == 5:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(str(fermMarketList[i].zip)))
-                elif j == 6:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(f"{fermMarketList[i].x:.1f}"))
-                elif j == 7:
-                    self.MarketTable.setItem(i,j, QtWidgets.QTableWidgetItem(f"{fermMarketList[i].y:.1f}"))
-
-        self.MarketTable.resizeColumnsToContents()
+        if self.searchByCityState.isChecked():
+            city = self.inputCity.toPlainText()
+            state = self.inputState.toPlainText()
+            search_list = model.srchBycityandstate(fermMarketList, city, state)
+            self.ShowMarkets(search_list)
+        elif self.searchByZip.isChecked():
+            zip = self.inputZip.toPlainText()
+            search_list = model.srchByZip(fermMarketList, zip)
+            self.ShowMarkets(search_list)
 
 
 
